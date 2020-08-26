93ea59185b1b008e5da41a5539a52d5e06b9b13f
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2020-08-25 21:44:01 -0400

    final build

---
+++ precache-manifest.3cdba29e8d1ad531651ddf9f7c531e2d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348aa6c928802efa08658e10ea652997",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "929917eeadb06cdd5db7",
+    "url": "/portfolio/static/css/main.5f567e1e.chunk.css"
+  },
+  {
+    "revision": "0d48230c1f574f6f67dc",
+    "url": "/portfolio/static/js/2.3ddb3eb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3ddb3eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929917eeadb06cdd5db7",
+    "url": "/portfolio/static/js/main.ca96e4d7.chunk.js"
+  },
+  {
+    "revision": "262d1be7e84afd072ed5",
+    "url": "/portfolio/static/js/runtime-main.0e20f7ea.js"
+  },
+  {
+    "revision": "aa739dcd346a9bafbe0005ba6e78815b",
+    "url": "/portfolio/static/media/NamePlate.aa739dcd.svg"
+  },
+  {
+    "revision": "eaacd67ecba66d1ce10c91b3b992061a",
+    "url": "/portfolio/static/media/checkpoint-controller-hover.eaacd67e.gif"
+  },
+  {
+    "revision": "fffeaaf872c04e82c523e1f87cef3b16",
+    "url": "/portfolio/static/media/checkpoint-controller.fffeaaf8.png"
+  },
+  {
+    "revision": "3626cef679b2fd64e05350c89093156b",
+    "url": "/portfolio/static/media/covid-peach-hover.3626cef6.gif"
+  },
+  {
+    "revision": "086400cbee10ba549c33b88622ecf05d",
+    "url": "/portfolio/static/media/covid-peach.086400cb.png"
+  },
+  {
+    "revision": "234214f58674baa5b27a8bde47f931c9",
+    "url": "/portfolio/static/media/headshot2.234214f5.jpg"
+  },
+  {
+    "revision": "3bc26c73b719bb374d3c19a82407dc56",
+    "url": "/portfolio/static/media/weather-sun-hover.3bc26c73.gif"
+  },
+  {
+    "revision": "39de34977b0e3d912ae11738e59d5ede",
+    "url": "/portfolio/static/media/weather-sun.39de3497.png"
+  }
+]);
